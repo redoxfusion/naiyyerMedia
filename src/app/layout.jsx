3c3821d35b57ff09ce2984a8dfe32560cc65a459
redoxfusion <khubaibkaramat1@gmail.com>
2025-04-21 11:49:55 +0500

    21th again  fixed insights next

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,8 +10,7 @@ export default function RootLayout({ children }) {
        <NevBar />
         {children}
        <Footer />
-        <SpeedInsights>
-        </SpeedInsights>
+        <SpeedInsights/>
       </body>
     </html>
   );
